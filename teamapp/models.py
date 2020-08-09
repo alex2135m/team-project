96148f0ce196667a705c2d93bc7827f2435fa468
admin <admin@admin.com>
2020-08-09 14:54:30 +0300

    New changes.

--- teamapp/models.py
+++ teamapp/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class List(models.Model):
     title = models.CharField('Название', max_length=100)
     description = models.TextField('Описание')
-    rating = models.IntegerField('Рейтинг')
+    rating = models.FloatField('Рейтинг')
 
     def __str__(self):
         return self.title

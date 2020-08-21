74fa7883b6e6f16405385f7b482aad46b433b597
alex2135m <66020856+alex2135m@users.noreply.github.com>
2020-08-21 17:55:05 +0300

    Кажадая запись является уникальной.

--- teamapp/models.py
+++ teamapp/models.py
@@ -5,10 +5,10 @@ from django.conf import settings
 # представление строк в БД
 class List(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.CASCADE)
-    title = models.CharField('Название', max_length=100)
-    description = models.TextField('Описание')
-    address = models.CharField('Адрес ресторана', max_length=150, null=True)
-    mailbox = models.EmailField('E-mail', max_length=100, null=True)
+    title = models.CharField('Название', max_length=100, unique=True)
+    description = models.TextField('Описание', unique=True)
+    address = models.CharField('Адрес ресторана', max_length=150, null=True, unique=True)
+    mailbox = models.EmailField('E-mail', max_length=100, null=True, unique=True)
     rating = models.FloatField('Рейтинг')
     objects = models.Manager()
 

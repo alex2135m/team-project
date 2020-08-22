d45fccc68da89cfdf0445d54ff6f9a9b4683aafc
alex2135m <66020856+alex2135m@users.noreply.github.com>
2020-08-22 13:45:16 +0300

    Заглавная буква автоматически выставляется.

--- teamapp/forms.py
+++ teamapp/forms.py
@@ -5,6 +5,12 @@ from .models import List
 # создание формы, связанную с моделью List
 class ListForm(ModelForm):
 
+    # функция, которая автоматически делает название с заглавной буквой
+    def clean_title(self):
+        return self.cleaned_data['title'].capitalize()
+
+    # параметры формы,
+    # поля, которые присутствуют в форме
     class Meta:
         model = List
         fields = ('author', 'title', 'description', 'address', 'mailbox', 'rating')

29f4c8303d69b6df09ca69eec0fde8d27d3eb1c5
alex2135m <66020856+alex2135m@users.noreply.github.com>
2020-08-24 15:51:54 +0300

    Новые изменения.

--- teamapp/models.py
+++ teamapp/models.py
@@ -4,7 +4,8 @@ from django.conf import settings
 
 # представление строк в БД
 class List(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, default=True, null=True, verbose_name='Автор', on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, default=True, null=True, verbose_name='Автор',
+                               on_delete=models.CASCADE)
     title = models.CharField('Название', max_length=100, unique=True)
     description = models.TextField('Описание', unique=True)
     address = models.CharField('Адрес ресторана', max_length=150, null=True, unique=True)
@@ -16,6 +17,7 @@ class List(models.Model):
     def __str__(self):
         return self.title
 
+# функция, которая делает верхний регистр в поле title
     def save(self, *args, **kwargs):
         self.title = self.title.capitalize()
         return super(List, self).save(*args, **kwargs)
